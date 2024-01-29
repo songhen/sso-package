5dd31adf881cd195075185ad21d0aeb4c495daac
odom.chan <odom.chan@wingmoney.com>
2024-01-29 11:14:49 +0700

    feat: payment sdk skeleton

--- packages/payment/src/index.ts
+++ packages/payment/src/index.ts
@@ -1,3 +1,46 @@
 import ApiService from 'wing-b2c-core-sdk/api'
+import { AxiosInstance } from 'axios'
+import path from 'path'
+import fs from 'fs'
 
-export class PaymentSDK extends ApiService {}
+export class PaymentSDK extends ApiService {
+  private static instance: PaymentSDK
+
+  private constructor() {
+    const filePath = path.resolve(__dirname, 'config.json')
+    const options = JSON.parse(fs.readFileSync(filePath, 'utf8'))
+
+    super({
+      baseURL: options.baseURL,
+      timeout: options.timeout,
+    })
+  }
+
+  private static getInstance(): AxiosInstance {
+    if (!PaymentSDK.instance) {
+      PaymentSDK.instance = new PaymentSDK()
+    }
+
+    return PaymentSDK.instance.apiInstance
+  }
+
+  static getPaymentProviders() {}
+
+  static updatePaymentProvider() {}
+
+  static saveDefaultPaymentProvider() {}
+
+  static createPayment() {}
+
+  static createRepayment() {}
+
+  static checkPaymentStatus() {}
+
+  static saveCardToken() {}
+
+  static pay() {}
+
+  private payWithProvider() {}
+
+  static canPayWithProvider() {}
+}
